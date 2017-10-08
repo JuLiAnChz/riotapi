e211cac30676da222420e9851b2f11eab390636c
Julian Andres Chilito <julian.chilitoz@gmail.com>
2017-10-07 19:34:01 -0500

    Initial Commit

---
+++ src/RiotApi.php
@@ -0,0 +1,12 @@
+<?php
+namespace JuLiAnChz\RiotApi;
+
+class RiotApi
+{
+	const REGION_GAME = 'la1';
+
+	public function helloWorld ()
+	{
+		return 'Hola Mundo!';
+	}
+}
---
+++ test/RiotApiTest.php
@@ -0,0 +1,10 @@
+<?php
+use PHPUnit_Framework_TestCase as PHPFT;
+
+class RiotApiTest extends PHPFT
+{
+	public function nothingTest ()
+	{
+		$this->assertTre (true);
+	}
+}
